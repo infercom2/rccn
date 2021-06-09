7a3595747f9fe31815ffc1fc9d44079a56efb67f
Keith Whyte <keith@rhizomatica.org>
2021-06-09 03:40:12 +0200

    Pure WS

--- rai/login.php
+++ rai/login.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 require_once('modules/session.php');
 $sess = new Session(true);
@@ -25,7 +25,7 @@ require_once('include/header.php');
 	});		
 
 	</script>
-			<br/><br/><br/><br/>
+	<br/><br/><br/><br/>
 
 <?php
 	function print_form($post_data, $errors, $language) {
@@ -38,33 +38,33 @@ require_once('include/header.php');
 			$_code[$language] = "_".$langcode;
 		}
 ?>
-			<form action="login.php" method="post" id="newRequestForm">
-			<fieldset class="formLogin">
-			    <h2></h2>
-			    <div style='margin:auto; width:130px; color: red; font-size: 12px;'><?= nl2br($errors) ?></div><br/>
-			    <div>
-			        <label for="user_login"><?=_('Username:')?> &nbsp;</label>
-				<input type='text' name='username' size='15' />
-			    </div>
-			    <div>
-			        <label for="user_password"><?=_('Password:')?> &nbsp;</label>
-				<input type='password' name='password' size='15' />
-			    </div>
-			    <div>
-				<br/>
-				<input type="hidden" name="language" id="language" value="" />
-				<select id="language-select">
-					<option value="es" <?=($language=='es') ? 'selected="selected"' : ''?>data-imagesrc="img/es<?=$_code['es']?>_flag.png">Español</option>
-					<option value="pt_BR" <?=($language=='pt_BR') ? 'selected="selected"' : ''?>data-imagesrc="img/br_flag.png">Português</option>
-					<option value="en" <?=($language=='en') ? 'selected="selected"' : ''?>data-imagesrc="img/en<?=$_code['en']?>_flag.png">English</option>
-				</select>
-			    </div>
-			    <div><br/>
-			        <label>&nbsp;</label>
-			        <input type="submit" name="access" class='login_button' value="<?=_('Login')?>" />
-			    </div>
-			</fieldset>
-			</form>
+<form action="login.php" method="post" id="newRequestForm">
+<fieldset class="formLogin">
+    <h2></h2>
+    <div style='margin:auto; width:130px; color: red; font-size: 12px;'><?= nl2br($errors) ?></div><br/>
+    <div>
+        <label for="user_login"><?=_('Username:')?> &nbsp;</label>
+	<input type='text' name='username' size='15' />
+    </div>
+    <div>
+        <label for="user_password"><?=_('Password:')?> &nbsp;</label>
+	<input type='password' name='password' size='15' />
+    </div>
+    <div>
+	<br/>
+	<input type="hidden" name="language" id="language" value="" />
+	<select id="language-select">
+		<option value="es" <?=($language=='es') ? 'selected="selected"' : ''?>data-imagesrc="img/es<?=$_code['es']?>_flag.png">Español</option>
+		<option value="pt_BR" <?=($language=='pt_BR') ? 'selected="selected"' : ''?>data-imagesrc="img/br_flag.png">Português</option>
+		<option value="en" <?=($language=='en') ? 'selected="selected"' : ''?>data-imagesrc="img/en<?=$_code['en']?>_flag.png">English</option>
+	</select>
+    </div>
+    <div><br/>
+        <label>&nbsp;</label>
+        <input type="submit" name="access" class='login_button' value="<?=_('Login')?>" />
+    </div>
+</fieldset>
+</form>
 <?php
 
 	}
@@ -86,12 +86,12 @@ require_once('include/header.php');
 	}
 
 	if (isset($_POST['access']) && $error_txt != "") {
-		print_form(1,$error_txt, substr($language,0,2));
+		print_form(1, $error_txt, substr($language,0,2));
 	} else {
 		print_form(0, '', substr($language,0,2));
 	}
 
 ?>
-	</div>
-	</body>
+</div>
+</body>
 </html>
