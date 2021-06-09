65a2f5e1e5b53ba4c1dc290b34be56e412003ea2
Keith Whyte <keith@rhizomatica.org>
2021-06-09 03:24:14 +0200

    Add an ICON and make the header image a link to index.php

--- rai/include/header.php
+++ rai/include/header.php
@@ -13,34 +13,40 @@ header('Content-Type: text/html; charset=utf-8');
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
-        <head>
-                <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <title><?=_('RAI - Rhizomatica Administration Interface')?></title>
 
-                <title><?=_('RAI - Rhizomatica Administration Interface')?></title>
-                <style type="text/css" title="currentStyle">
-                @import "css/page.css";
-                @import "css/table.css";
-			    @import "css/login.css";
-			    @import "css/form.css";
-                @import "js/fancybox/jquery.fancybox-1.3.4.css";
-                </style>
-                <link rel="stylesheet" href="css/jquery-ui.min.css" type="text/css" />
-                <link rel="stylesheet" type="text/css" href="css/dd.css" />
-                <script type="text/javascript" language="javascript" src="js/jquery.js"></script>
-                <script type="text/javascript" language="javascript" src="js/jquery-ui.min.js"></script>
-                <script type="text/javascript" language="javascript" src="js/jquery.dataTables.js"></script>
-                <script type="text/javascript" language="javascript" src="js/jquery.dataTables.columnFilter.js"></script>
-                <script type="text/javascript" language="javascript" src="js/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
-                <script type="text/javascript" language="javascript" src="js/jquery.dd.min.js"></script>
-                <script type="text/javascript" language="javascript" src="js/rai.js"></script>
-        <script type="text/javascript" language="javascript">
-                 <? include("js/js_locale.php");?>
-        </script>
-        </head>
+        <style type="text/css" title="currentStyle">
+            @import "css/page.css";
+            @import "css/table.css";
+            @import "css/login.css";
+            @import "css/form.css";
+            @import "js/fancybox/jquery.fancybox-1.3.4.css";
+        </style>
+
+        <link rel="stylesheet" href="css/jquery-ui.min.css" type="text/css" />
+        <link rel="stylesheet" type="text/css" href="css/dd.css" />
+        <link rel="icon" href="favicon.ico">
+        <script type="text/javascript" language="javascript" src="js/jquery.js"></script>
+        <script type="text/javascript" language="javascript" src="js/jquery-ui.min.js"></script>
+        <script type="text/javascript" language="javascript" src="js/jquery.dataTables.js"></script>
+        <script type="text/javascript" language="javascript" src="js/jquery.dataTables.columnFilter.js"></script>
+        <script type="text/javascript" language="javascript" src="js/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
+        <script type="text/javascript" language="javascript" src="js/jquery.dd.min.js"></script>
+        <script type="text/javascript" language="javascript" src="js/rai.js"></script>
+    <script type="text/javascript" language="javascript">
+        <? include("js/js_locale.php");?>
+    </script>
+    </head>
         <body id="dt_example">
-                <div id="container">
-			<? if (!isset($no_title)) { ?>
-                        <div class="full_width big">
-                                <img src="img/rhizomatica_logo_small.png" style="vertical-align: middle;" /> <?=_('RAI - Rhizomatica Administration Interface')?>
-                        </div>
-			<? } ?>
+            <div id="container">
+
+<? if (!isset($no_title)) { ?>
+
+        <div class="full_width big">
+            <a href="index.php">
+                <img src="img/rhizomatica_logo_small.png" style="vertical-align: middle;" /></a> <?=_('RAI - Rhizomatica Administration Interface')?>
+        </div>
+
+<? } ?>
