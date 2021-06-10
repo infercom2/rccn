22377376946ba3542db5f4b048660df3c7fb308d
Keith Whyte <keith@rhizomatica.org>
2021-06-10 22:45:15 +0200

    Stats: update network script for osmo-bsc

--- bin/network_update60.sh
+++ bin/network_update60.sh
@@ -30,7 +30,24 @@ echo $mmc > /tmp/mm_contexts
 rrdtool update $RHIZO_DIR/mm_contexts.rrd N:$mmc
 
 for bts in $mybts ; do
-  eval _channels_$bts=`echo "show bts $bts" | nc -q1 localhost 4242 | awk 'BEGIN {tch=0;sdcch=0} /TCH\// {gsub("\\\(|\\\)","",$3) split($3,a,"\\\/"); tch=a[1]}; /SDCCH/ { gsub("\\\(|\\\)","",$3) split($3,a,"\\\/"); sdcch+=a[1] } END {print tch":"sdcch}'`
+  if [ "$OSMO_STACK" == "split" ] ;then
+
+    _bts_vty=`echo "show bts $bts" | nc -q1 localhost 4242 | grep 'Number.*channels used'`
+    eval _channels_$bts=`echo -e "$_bts_vty" | \
+     awk 'BEGIN { tchF=0; tchH=0; tchD=0; sdcch4=0; sdcch8=0; } \
+     /TCH\/F channels used/ {tchF=$6}; \
+     /TCH\/H channels used/ {tchH=$6}; \
+     /TCH\/F_TCH\/H_PDCH channels used/ {tchD=$6}; \
+     /SDCCH4 channels used/ {sdcch4=$6}; \
+     /SDCCH8 channels used/ {sdcch8=$6}; \
+     END { print tchF+tchH+tchD":"sdcch4+sdcch8 }'`
+
+  else
+
+    eval _channels_$bts=`echo "show bts $bts" | nc -q1 localhost 4242 |\
+     awk 'BEGIN {tch=0;sdcch=0} /TCH\// {gsub("\\\(|\\\)","",$3) split($3,a,"\\\/"); tch=a[1]}; /SDCCH/ { gsub("\\\(|\\\)","",$3) split($3,a,"\\\/"); sdcch+=a[1] } END {print tch":"sdcch}'`
+
+  fi
 done
 rrdtool update $RHIZO_DIR/bts_channels60.rrd N:$_channels_0:$_channels_1:$_channels_2:$_channels_3:$_channels_4:$_channels_5
 
