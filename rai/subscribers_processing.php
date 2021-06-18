8cbf6c787b0af44a8f60af93bd2e107cda78dc84
Keith Whyte <keith@rhizomatica.org>
2021-06-19 01:09:26 +0200

    Last commit: correct typo

--- rai/subscribers_processing.php
+++ rai/subscribers_processing.php
@@ -240,7 +240,7 @@
             !in_array($aRow['msisdn'],$sip) ) {
             continue;
         }
-        if ($rai_filter == 'RAI-all-roaming' && ($aRow['current_bts'] == $aRow['home_bts'] || $aRow['authorised'] != 1) ) {
+        if ($rai_filter == 'RAI-all-roaming' && ($aRow['current_bts'] == $aRow['home_bts'] || $aRow['authorized'] != 1) ) {
             continue;
         }
 
