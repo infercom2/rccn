ac542313d1589f10cd071f686b486fc2c66c4c7a
Keith Whyte <keith@rhizomatica.org>
2021-11-06 21:52:25 +0100

    SMS Cleanup, Split Stack.
    When cleaning up SMS on the split stack, attach the hlr database and
    query for subscribers that have not been seen/are not authorised
    so that we can quickly delete broadcast messages directed to them and
    not choke up the queue.

--- bin/sms_cleanup.sh
+++ bin/sms_cleanup.sh
@@ -32,8 +32,16 @@ logc "Total SMS: $total_sms Delivered: $total_sms_delivered SMS older than 4 day
 logc "Cleanup DB"
 
 # Delete Any Broadcast SMS to a subscriber that is currently not authorised and also has not been seen for two weeks.
-echo "DELETE from SMS where src_ton=5 and exists (select * from subscriber where subscriber.extension = sms.dest_addr AND subscriber.expire_lu < datetime('now', '-14 day') and subscriber.authorized = 0);" | sqlite3 -init <(echo .timeout 1000) $SMS_DB
+if [ "$OSMO_STACK" == "split" ] ; then
+  echo "ATTACH \"/var/lib/osmocom/hlr.db\" as hlr; \
+  DELETE from SMS where src_ton=5 and exists (select msisdn,nam_cs,last_lu_seen \
+  from hlr.subscriber where msisdn = sms.dest_addr and last_lu_seen < datetime('now', '-14 day') and nam_cs = 0);" | sqlite3 -init <(echo .timeout 1000) $SMS_DB
+else
+  echo "DELETE from SMS where src_ton=5 and exists (select * from subscriber where subscriber.extension = sms.dest_addr AND subscriber.expire_lu < datetime('now', '-14 day') and subscriber.authorized = 0);" | sqlite3 -init <(echo .timeout 1000) $SMS_DB
+fi
+# Delete any SMS in the database that was created more than 6 hours ago and has been sent.
 echo "DELETE from SMS where created < datetime(\"now\", \"-6 hours\") and sent is not null;" | sqlite3 -init <(echo .timeout 1000) $SMS_DB
+# Delete any broadcast message that is not delivered after 7 days.
 echo "DELETE from SMS where created < datetime(\"now\", \"-7 day\") and src_ton=5 and sent is null;" | sqlite3 -init <(echo .timeout 1000) $SMS_DB
 
 if [ "$OSMO_STACK" != "split" ] && [[ $(date +%u) == 1 ]]; then
